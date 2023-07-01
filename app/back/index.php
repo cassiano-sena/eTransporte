6de076f0940ae7d727ffae9bc5dc41069e2b4165
cassiano-sena <csdscr@gmail.com>
2023-06-30 23:26:31 -0300

    Delete app directory

--- app/back/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once('functions.php');
-    $api = new Api;
-    $api->processApi();
-
-    // senha: nao possui case-sensitivity
-
-    // addUsuario(): nao é inserido corretamente, provavelmente algum array/coluna/variável possui um valor diferente do que o necessario
